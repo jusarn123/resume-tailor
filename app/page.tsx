22256bdb15157991ee3b5fd0ef96df55f86f533f
justin <justin-arnold@live.com>
2025-05-09 19:42:09 -0400

    auto sync at 2025-05-09 19:42:09

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         </p>
       </div>
       */}
-      <div className="text-center space-y-4 mb-22">
+      <div className="text-center space-y-4 mb-10">
         <p className="text-lg font-light text-blue-100 max-w-xl mx-auto">
           Instantly shape your resume to fit any job description.
         </p>
