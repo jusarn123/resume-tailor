115cf1e5c73ac88e082bd0e86b7aec2b339f3f9b
justin <justin-arnold@live.com>
2025-05-09 13:37:07 -0400

    auto sync at 2025-05-09 13:37:06

--- components/nav.tsx
+++ components/nav.tsx
@@ -17,8 +17,8 @@ export const Nav = () => {
           <Image
             src="/avatar-placeholder.png"
             alt="User Avatar"
-            width={66}
-            height={66}
+            width={55}
+            height={55}
             className="rounded-full object-cover"
           />
         </div>
