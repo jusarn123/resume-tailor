161440c0546cc3c4b0c92b291e5c65d718e1d68a
justin <justin-arnold@live.com>
2025-05-09 19:35:57 -0400

    auto sync at 2025-05-09 19:35:57

--- components/nav.tsx
+++ components/nav.tsx
@@ -10,7 +10,7 @@ export const Nav = () => {
         {/* Logo Left */}
         <Link href="/" aria-label="Home" className="flex items-center ml-6">
           <Image src="/logo2_opt.png" alt="Logo" width={36} height={36} className="object-contain" />
-          <h1 className="ml-6 text-2xl md:text-6xl font-extrabold tracking-tight text-white text-center" style={{ fontFamily: 'Unbounded, sans-serif' }}>
+          <h1 className="ml-6 text-2xl md:text-5xl font-extrabold tracking-tight text-white text-center" style={{ fontFamily: 'Unbounded, sans-serif' }}>
             ResumeAI
           </h1>
         </Link>
