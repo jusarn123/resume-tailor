0094b8c84970efcaf46177038c30f3303e9ca32f
justin <justin-arnold@live.com>
2025-05-09 18:57:27 -0400

    auto sync at 2025-05-09 18:57:27

--- components/nav.tsx
+++ components/nav.tsx
@@ -13,7 +13,7 @@ export const Nav = () => {
         </Link>
 
         {/* Account Avatar Right */}
-        <div className="flex items-center mr-4">
+        
           <Image
             src="/avatar-placeholder.png"
             alt="User Avatar"
@@ -21,7 +21,7 @@ export const Nav = () => {
             height={55}
             className="rounded-full object-cover"
           />
-        </div>
+        
       </nav>
     </aside>
   );
