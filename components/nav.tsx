d56685240d02af37da6fdb5743d35a7e04c31d00
justin <justin-arnold@live.com>
2025-05-09 13:35:11 -0400

    auto sync at 2025-05-09 13:35:11

--- components/nav.tsx
+++ components/nav.tsx
@@ -6,14 +6,14 @@ import Image from 'next/image';
 export const Nav = () => {
   return (
     <aside className="w-full bg-transparent z-50">
-      <nav className="flex items-center justify-between px-2 sm:px-4 py-2">
+      <nav className="flex items-center justify-between px-0 sm:px-2 py-2">
         {/* Logo Left */}
-        <Link href="/" aria-label="Home" className="flex items-center ml-1">
-          <Image src="/logo.png" alt="Logo" width={72} height={72} className="object-contain" />
+        <Link href="/" aria-label="Home" className="flex items-center ml-0">
+          <Image src="/logo.png" alt="Logo" width={88} height={88} className="object-contain" />
         </Link>
 
         {/* Account Avatar Right */}
-        <div className="flex items-center mr-1">
+        <div className="flex items-center mr-2">
           <Image
             src="/avatar-placeholder.png"
             alt="User Avatar"
