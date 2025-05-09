1656fa30ceb268cc168bbf0a1bba7fe68f5256a3
justin <justin-arnold@live.com>
2025-05-08 23:58:44 -0400

    auto sync at 2025-05-08 23:58:44

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.pn",
+    shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
 };
