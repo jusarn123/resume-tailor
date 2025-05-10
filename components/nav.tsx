07f5007bd880042ecf70ba74d5887e006d11f830
justin <justin-arnold@live.com>
2025-05-09 21:16:55 -0400

    auto sync at 2025-05-09 21:16:55

--- components/nav.tsx
+++ components/nav.tsx
@@ -8,13 +8,19 @@ export const Nav = () => {
     <aside className="w-full bg-transparent z-50">
       <nav className="flex items-center justify-between px-0 sm:px-2 py-4">
         {/* Logo Left */}
-        <Link href="/" aria-label="Home" className="flex items-center ml-4">
-          <Image src="/logo2_opt.png" alt="Logo" width={30} height={30} className="object-contain w-6 md:w-9" />
-          <div className="text-center space-y-2 md:space-y-4 mb-8">
+        <Link href="/" aria-label="Home" className="flex items-center gap-4 ml-4">
+          <Image 
+            src="/logo2_opt.png" 
+            alt="Logo" 
+            width={30} 
+            height={30} 
+            className="object-contain w-6 md:w-9"
+          />
+          <div className="flex flex-col justify-center text-center">
             <h2 className="text-2xl md:text-5xl tracking-tight text-white" style={{ fontFamily: 'Unbounded, sans-serif' }}>
               ResumeAI
             </h2>
-            <p className="text-xl md:text-4xl font-light text-blue-100 mb-3 md:mb-12">
+            <p className="text-xl md:text-4xl font-light text-blue-100 mb-0 md:mb-2">
               <span className="extrabold">Drop</span> it. Tailor it. Apply.
             </p>
           </div>
