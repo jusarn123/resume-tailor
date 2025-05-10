ebab330ab65f87f451b5236b470e7c0ce1658b68
justin <justin-arnold@live.com>
2025-05-09 20:11:07 -0400

    auto sync at 2025-05-09 20:11:07

--- components/nav.tsx
+++ components/nav.tsx
@@ -22,7 +22,7 @@ export const Nav = () => {
             alt="User Avatar"
             width={44}
             height={44}
-            className="rounded-full object-cover mr-5 w-11 md:w-15"
+            className="rounded-full object-cover mr-4 w-11 md:w-15"
           />
         
       </nav>
