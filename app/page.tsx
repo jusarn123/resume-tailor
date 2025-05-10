540474fe6ea66d144638bd07416bf9d4c1edcfbc
justin <justin-arnold@live.com>
2025-05-09 20:41:36 -0400

    auto sync at 2025-05-09 20:41:36

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       </div>
       */}
       <div className="text-center space-y-4 mb-8">
-        <p className="text-xl md:text-4xl font-light text-blue-100 mb-3 md:mb-6">
+        <p className="text-xl md:text-4xl font-light text-blue-100 mb-3 md:mb-12">
           Drop it. Tailor it. Apply.
         </p>
       </div>
